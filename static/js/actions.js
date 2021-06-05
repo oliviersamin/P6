c768d5074681ffba3c265f1f08f3cdaa6b52bb18
Olivier SAMIN <oliviersamin@gmail.com>
2021-06-05 23:00:59 +0100

    new features: 1. bug fixed: movies added to Categories.movies array 2. movies cover displayed on start for each category

--- static/js/actions.js
+++ static/js/actions.js
@@ -10,13 +10,11 @@ let url_get_best_movies = 'http://localhost:8000/api/v1/titles?sort_by=imdb_scor
 // class to define a movie with all the attributs asked by client
 class Movie {
     constructor(url) {
-//    console.log('dans constructeur: ' + json.title);
         this.url = url
         this.id = "";
         this.image_url = "";
         this.title = "";
         this.genres = "";
-//        console.log(this.title+ ' ' + this.cover);
         this.year= "";
         this.rated = "";
         this.imdb_score = "";
@@ -26,15 +24,13 @@ class Movie {
         this.countries = "";
         this.worldwide_gross_income = "";
         this.description = "";
-//        this.display_attributes();
-//        this.set_info();
     }
 
     async set_info() {
         const response = await fetch(this.url);
         var data = await response.json();
         this.set_attributes(data);
-        this.display_attributes();
+//        this.display_attributes();
 
     }
 
@@ -68,24 +64,28 @@ class Categories {
         this.urls_movies = [];
         this.movies = [];
         await this.getLastPagesFromAPI();
-        console.log('dans constructor apres getLastPagesFromAPI');
+//        console.log('dans constructor apres getLastPagesFromAPI');
         await this.set_all_movies_info();
-        console.log('dans constructor apres set_all_movies_info');
+//        console.log('dans constructor apres set_all_movies_info');
         return this;
         })();
     }
 
     async set_all_movies_info() {
 //        await this.getLastPagesFromAPI();
-        console.log('dans set_all_movies_info()')
+//        console.log('dans set_all_movies_info()')
         for (let index=0; index < this.urls_movies.length; index++){
-            let data = await this.getRawInfoFromApi(this.urls_movies[index]);
-            console.log('index = ' + index + '  title = ' + data.title + '  imdb = ' + data.imdb_score)
+//            let data = await this.getRawInfoFromApi(this.urls_movies[index]);
+            const movie = new Movie(this.urls_movies[index])
+            await movie.set_info()
+            this.movies.push(movie)
+
+//            console.log('index = ' + index + '  title = ' + data.title + '  imdb = ' + data.imdb_score)
         }
     }
 
     async getLastPagesFromAPI() {
-    console.log('dans getLastPagesFromAPI()')
+//    console.log('dans getLastPagesFromAPI()')
     var data = await this.getRawInfoFromApi(this.url_start)
     this.nb_pages = Math.ceil(data.count/data.results.length);
 //    console.log(this.nb_pages, Math.ceil(this.nb_pages));
@@ -97,36 +97,34 @@ class Categories {
     }
 
     async get_all_urls_movies(data) {
-        console.log('dans get_all_urls_movies() ')
+//        console.log('dans get_all_urls_movies() ')
         this.loopToGetUrlsFromPage(data);
         if (this.check_if_get_all_urls_movies()) {
             console.log('toutes les urls sont récupérées')
         } else {
-            console.log('dans else')
+//            console.log('dans else')
            var info = await this.getRawInfoFromApi(data.previous);
-           console.log(info.results);
+//           console.log(info.results);
            return this.get_all_urls_movies(info)
         }
 //    console.log('fin de get_all_urls_movies : ' + this.urls_movies.length + '   ' + this.urls_movies)
     }
 
-
     check_if_get_all_urls_movies(){
-    console.log('dans check_if_get_all_urls_movies: ' + this.urls_movies.length + ' ' +  this.length)
+//    console.log('dans check_if_get_all_urls_movies: ' + this.urls_movies.length + ' ' +  this.length)
     if (this.urls_movies.length < this.length) {
         let count = this.length - this.urls_movies.length;
-        console.log('il manque ' + count + ' urls');
+//        console.log('il manque ' + count + ' urls');
         return false
     } else {
         return true
     }
     }
 
-
     async check_if_last_page(pageJson) {
-    console.log('dans check_if_last_page()')
+//    console.log('dans check_if_last_page()')
     if (pageJson.next == null) {
-        console.log('dernière page, il y a ' + pageJson.results.length + ' films')
+//        console.log('dernière page, il y a ' + pageJson.results.length + ' films')
         return pageJson
 //        console.log(data2.results);
     } else {
@@ -136,10 +134,10 @@ class Categories {
     }
 
     loopToGetUrlsFromPage(pageJson) {
-    console.log('dans LoopToGetUrlFromPage : ' + this.length + ' ' + this.urls_movies.length)
+//    console.log('dans LoopToGetUrlFromPage : ' + this.length + ' ' + this.urls_movies.length)
     let urls = [];
     let urls_count = this.length - this.urls_movies.length;
-    console.log('urls_count = ' + urls_count)
+//    console.log('urls_count = ' + urls_count)
     for (let index=0; index<pageJson.results.length;index++){
         if (urls_count != 0) {
             urls.push(pageJson.results[index].url);
@@ -150,11 +148,11 @@ class Categories {
     for (let i in urls){
         this.urls_movies.push(urls[i]);
     }
-    console.log('this_urls_movies = '  + this.urls_movies.length + ' ' + this.urls_movies)
+//    console.log('this_urls_movies = '  + this.urls_movies.length + ' ' + this.urls_movies)
 }
 
     async getRawInfoFromApi(url){
-        console.log('dans getRawInfoFromApi()')
+//        console.log('dans getRawInfoFromApi()')
         const response = await fetch(url);
         var data = await response.json();
         return data
@@ -176,17 +174,49 @@ function right_arrow() {
 console.log('fonction right_arrow OK');
 }
 
+function best_movie_display(movie_category) {
+    const best_movie_cover = document.getElementById("best_movie_cover");
+    const best_movie_title = document.getElementById("best_movie_title");
+//    console.log(movie_category.movies[0].image_url, movie_category.movies[0].title);
+    best_movie_cover.innerHTML = "<div id='best_movie_cover'><img src=" + movie_category.movies[0].image_url + " alt='' /></div>";
+    best_movie_title.innerHTML = "<div id='best_movie_title'>" + movie_category.movies[0].title + "</div>";
+}
+
+function basic_display(movie_category, rank) {
+    const movies_html_class = document.getElementsByClassName("movies");
+//    console.log(movies_html_class)
+    display_movies_in_section(movies_html_class[rank], movie_category);
+}
+
+function display_movies_in_section(arg, movie_category) {
+    const children = arg.children;
+    let m = 0;
+    for (let i=1; i< children.length - 1 ; i++) {
+            if (movie_category.movies.length ==7){
+                m = i -1 ;
+            } else {
+                m = i;
+            }
+//        console.log(m, movie_category.movies[m].genres, movie_category.movies[m].imdb_score)
+        children[i].innerHTML = "<div class='movie'><img src=" + movie_category.movies[m].image_url + " alt='' /></div>"
+    }
+}
 
 async function main() {
-    console.log('début de main...')
-    const category_comedy = await new Categories(url_get_comedy_movies,7);
-    console.log('CATEGORY_COMEDY SAVED IN CLASS')
-    const category_family = await new Categories(url_get_family_movies,7);
-    console.log('CATEGORY_FAMILY SAVED IN CLASS')
-    const category_animation = await new Categories(url_get_animation_movies,7);
-    console.log('CATEGORY_ANIMATION SAVED IN CLASS')
+    console.log('début de main...');
     const category_best = await new Categories(url_get_best_movies,8);
-    console.log('CATEGORY_BEST SAVED IN CLASS')
+    console.log('CATEGORY_BEST SAVED IN CLASS');
+    best_movie_display(category_best);
+    basic_display(category_best,0);
+    const category_comedy = await new Categories(url_get_comedy_movies,7);
+    console.log('CATEGORY_COMEDY SAVED IN CLASS');
+    basic_display(category_comedy,1);
+    const category_family = await new Categories(url_get_family_movies,7);
+    console.log('CATEGORY_FAMILY SAVED IN CLASS');
+    basic_display(category_family,2);
+    const category_animation = await new Categories(url_get_animation_movies,7);
+    console.log('CATEGORY_ANIMATION SAVED IN CLASS');
+    basic_display(category_animation,3);
     console.log('fin de main....');
 }
 
